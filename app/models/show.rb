612711038ec3958674411bd22724e40fb50f7c69
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-07-30 18:40:47 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,32 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    rating = self.highest_rating
+    self.where(rating: rating).first
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    rating = self.lowest_rating
+    self.where(rating: rating).first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
+
+end
